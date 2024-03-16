6e81abd28d06780b6ee245c397b598d68875d6cd
Bill Gauvey <bgauvey@gmail.com>
2024-03-15 22:07:39 -0400

    fixed namespaces

--- Src/mes-api/mes-api/Controllers/AuthorizationController.cs
+++ Src/mes-api/mes-api/Controllers/AuthorizationController.cs
@@ -3,34 +3,33 @@ using BOL.API.Service.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace bol.api.Controllers
+namespace bol.api.Controllers;
+
+[Route("auth")]
+public class AuthorizationController : ControllerBase
 {
-    [Route("auth")]
-    public class AuthorizationController : ControllerBase
+    readonly BOL.API.Service.Interfaces.IAuthorizationService _authorizationService;
+    readonly ILogger _logger;
+
+    public AuthorizationController(BOL.API.Service.Interfaces.IAuthorizationService authorizationService, ILoggerFactory loggerFactory)
     {
-        readonly BOL.API.Service.Interfaces.IAuthorizationService _authorizationService;
-        readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger(nameof(AuthorizationController));
+        _authorizationService = authorizationService;
+    }
 
-        public AuthorizationController(BOL.API.Service.Interfaces.IAuthorizationService authorizationService, ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger(nameof(AuthorizationController));
-            _authorizationService = authorizationService;
-        }
+    // POST: auth/login
+    [AllowAnonymous]
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(string), 200)]
+    [ProducesResponseType(typeof(string), 400)]
+    public async Task<IActionResult> Login([FromBody] AuthenticateModel model)
+    {
+        var user = await _authorizationService.Login(model);
 
-        // POST: auth/login
-        [AllowAnonymous]
-        [HttpPost("login")]
-        [ProducesResponseType(typeof(string), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> Login([FromBody] AuthenticateModel model)
-        {
-            var user = await _authorizationService.Login(model);
+        if (user == null)
+            return BadRequest(new { message = "Username or password is incorrect" });
 
-            if (user == null)
-                return BadRequest(new { message = "Username or password is incorrect" });
-
-            return Ok(user.Token);
-        }
+        return Ok(user.Token);
     }
 }
 
--- Src/mes-api/mes-api/Controllers/EntController.cs
+++ Src/mes-api/mes-api/Controllers/EntController.cs
@@ -3,69 +3,68 @@ using BOL.API.Service.Interfaces;
 using BOL.API.Service.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace bol.api.Controllers
+namespace bol.api.Controllers;
+
+[Route("ent")]
+public class EntController : Controller
 {
-    [Route("ent")]
-    public class EntController : Controller
+    private readonly ILogger _logger;
+    private readonly IEntService _entService;
+
+    public EntController(IEntService entService, ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
-        private readonly IEntService _entService;
+        _logger = loggerFactory.CreateLogger(nameof(EntController));
+        _entService = entService;
+    }
 
-        public EntController(IEntService entService, ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger(nameof(EntController));
-            _entService = entService;
-        }
+    // GET: api/values
+    [HttpGet]
+    public IEnumerable<Ent> Get()
+    {
+        return _entService.GetAll();
+    }
 
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<Ent> Get()
-        {
-            return _entService.GetAll();
-        }
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public Ent Get(int id)
+    {
+        return _entService.GetEntById(id);
+    }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public Ent Get(int id)
-        {
-            return _entService.GetEntById(id);
-        }
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody]Ent entity)
+    {
+        _entService.Create(entity);
+    }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]Ent entity)
-        {
-            _entService.Create(entity);
-        }
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]Ent entity)
+    {
+        _entService.Update(entity);
+    }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Ent entity)
-        {
-            _entService.Update(entity);
-        }
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        _entService.Delete(id);
+    }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            _entService.Delete(id);
-        }
-
-        // GET ent/files/5
-        [HttpGet("files/{id}")]
-        public IEnumerable<EntFile> GetFiles(int id)
-        {
-            return _entService.GetFiles(id);
-        }
+    // GET ent/files/5
+    [HttpGet("files/{id}")]
+    public IEnumerable<EntFile> GetFiles(int id)
+    {
+        return _entService.GetFiles(id);
+    }
 
 
-        // GET ent/attrs/5
-        [HttpGet("attrs/{id}")]
-        public IEnumerable<EntityAttribute> GetAttrs(int id)
-        {
-            return _entService.GetAttrs(id);
-        }
+    // GET ent/attrs/5
+    [HttpGet("attrs/{id}")]
+    public IEnumerable<EntityAttribute> GetAttrs(int id)
+    {
+        return _entService.GetAttrs(id);
     }
 }
 
